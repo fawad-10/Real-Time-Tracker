2047cbf320507edc77276b9e24dede92ff64cc9a
Ahmed Fawad Awan <ahmadfawadawan@gmail.com>
2024-09-15 04:23:52 +0500

    ///

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 // Serve static files from the "public" directory
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   socket.on("send-location", (data) => {
